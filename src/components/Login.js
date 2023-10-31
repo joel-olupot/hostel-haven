96ae368419099d67d8352fa79f745bd6df3bc4c2
olupot-ikara <joel8tnz@gmail.com>
2023-10-31 15:54:40 +0300

    first modification to the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,38 +13,43 @@ const Login = () => {
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <div className="card mt-5">
-            <div className="card-header ">
+          <div className="card shadow mt-5 bg-white rounded">
+            <div className="card-header border-0">
               <h2 className="text-primary text-center">HOSTEL HAVEN</h2>
+              <p className="text-center mt-4">LOGIN TO YOUR ACCOUNT</p>
             </div>
             <div className="card-body">
               <form>
-                <div className="form-group">
-                  <label htmlFor="username">Username</label>
+                <div className="form-floating my-3">
                   <input
                     type="text"
                     className="form-control"
                     id="username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
-                    placeholder="Enter your username"
+                    placeholder=""
                   />
+                  <label htmlFor="username" className="form-label text-center">
+                    Username
+                  </label>
                 </div>
-                <div className="form-group">
-                  <label htmlFor="password">Password</label>
+                <div className="form-floating my-3">
                   <input
                     type="password"
                     className="form-control"
                     id="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter your password"
+                    placeholder=""
                   />
+                  <label htmlFor="password" className="form-label text-center">
+                    Password
+                  </label>
                 </div>
                 <div className="text-center my-2">
                   <button
                     type="button"
-                    className="btn btn-primary"
+                    className="btn btn-primary border-0"
                     onClick={handleLogin}
                   >
                     Login
