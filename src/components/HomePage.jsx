4597fcbeff0c075f6a39f8422e0c89a667328b35
olupot-ikara <joel8tnz@gmail.com>
2023-11-01 20:47:42 +0300

    Added the navbar to the login and home pages

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -25,28 +25,161 @@ function HomePage() {
       description: "Experience City 3 from Hostel C.",
       imageUrl: "https://via.placeholder.com/150",
     },
+    {
+      id: 4,
+      name: "Hostel D",
+      location: "City 3",
+      description: "Experience City 3 from Hostel D.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 5,
+      name: "Hostel E",
+      location: "City 3",
+      description: "Experience City 3 from Hostel E.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 6,
+      name: "Hostel F",
+      location: "City 3",
+      description: "Experience City 3 from Hostel F.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 7,
+      name: "Hostel G",
+      location: "City 3",
+      description: "Experience City 3 from Hostel G.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 8,
+      name: "Hostel H",
+      location: "City 3",
+      description: "Experience City 3 from Hostel H.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 9,
+      name: "Hostel I",
+      location: "City 3",
+      description: "Experience City 3 from Hostel I.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 10,
+      name: "Hostel J",
+      location: "City 3",
+      description: "Experience City 3 from Hostel J.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 11,
+      name: "Hostel K",
+      location: "City 3",
+      description: "Experience City 3 from Hostel K.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
+    {
+      id: 12,
+      name: "Hostel L",
+      location: "City 3",
+      description: "Experience City 3 from Hostel L.",
+      imageUrl: "https://via.placeholder.com/150",
+    },
   ];
 
   return (
-    <div className="container mt-4">
-      <h1 className="mb-4">Hostel Details</h1>
-      <div className="row">
-        {hostels.map((hostel) => (
-          <div key={hostel.id} className="col-md-4 mb-4">
-            <div className="card">
-              <img
-                src={hostel.imageUrl}
-                className="card-img-top"
-                alt={hostel.name}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{hostel.name}</h5>
-                <p className="card-text">{hostel.location}</p>
-                <p className="card-text">{hostel.description}</p>
+    <div>
+      <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            HOSTEL HAVEN
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a
+                  class="nav-link text-light active"
+                  aria-current="page"
+                  href="#"
+                >
+                  HOME
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">
+                  ABOUT
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">
+                  UPLOAD
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link text-light dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  ACCOUNT
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      SIGN IN
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      SIGN UP
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      LOG OUT
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container mt-4">
+        <div className="row">
+          {hostels.map((hostel) => (
+            <div key={hostel.id} className="col-md-3 mb-4">
+              <div className="card">
+                <img
+                  src={hostel.imageUrl}
+                  className="card-img-top"
+                  alt={hostel.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{hostel.name}</h5>
+                  <p className="card-text">{hostel.location}</p>
+                  <p className="card-text">{hostel.description}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,69 +10,146 @@ const Login = () => {
   };
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <div className="card shadow mt-5 bg-white rounded">
-            <div className="card-header border-0">
-              <h2 className="text-primary text-center">HOSTEL HAVEN</h2>
-              <p className="text-center mt-4">LOGIN TO YOUR ACCOUNT</p>
-            </div>
-            <div className="card-body">
-              <form>
-                <div className="form-floating my-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder=""
-                  />
-                  <label htmlFor="username" className="form-label text-center">
-                    Username
-                  </label>
-                </div>
-                <div className="form-floating my-3">
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder=""
-                  />
-                  <label htmlFor="password" className="form-label text-center">
-                    Password
-                  </label>
-                </div>
-                <div className="text-center my-2">
-                  <button
-                    type="button"
-                    className="btn btn-primary border-0"
-                    onClick={handleLogin}
-                  >
-                    Login
-                  </button>
-                </div>
-                <div class="container">
-                  <div class="row align-items-start">
-                    <div class="col-5">
-                      <button type="button" className="btn btn-primary">
-                        Forgot password?
-                      </button>
-                    </div>
-                    <div class="col-4">
-                      <p>Already have account?</p>
-                    </div>
-                    <div class="col-3">
-                      <button type="button" className="btn btn-primary">
-                        Sign Up
-                      </button>
+    <div>
+      <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            HOSTEL HAVEN
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a
+                  class="nav-link text-light active"
+                  aria-current="page"
+                  href="#"
+                >
+                  HOME
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">
+                  ABOUT
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-light" href="#">
+                  UPLOAD
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link text-light dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  ACCOUNT
+                </a>
+                <ul class="dropdown-menu">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      SIGN IN
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      SIGN UP
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      LOG OUT
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div className="card shadow mt-5 bg-white rounded">
+              <div className="card-header border-0">
+                <h2 className="text-primary text-center">HOSTEL HAVEN</h2>
+                <p className="text-center mt-4">LOGIN TO YOUR ACCOUNT</p>
+              </div>
+              <div className="card-body">
+                <form>
+                  <div className="form-floating my-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      placeholder=""
+                    />
+                    <label
+                      htmlFor="username"
+                      className="form-label text-center"
+                    >
+                      Username
+                    </label>
+                  </div>
+                  <div className="form-floating my-3">
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder=""
+                    />
+                    <label
+                      htmlFor="password"
+                      className="form-label text-center"
+                    >
+                      Password
+                    </label>
+                  </div>
+                  <div className="text-center my-2">
+                    <button
+                      type="button"
+                      className="btn btn-primary border-0"
+                      onClick={handleLogin}
+                    >
+                      Login
+                    </button>
+                  </div>
+                  <div class="container">
+                    <div class="row align-items-start">
+                      <div class="col-5">
+                        <button type="button" className="btn btn-primary">
+                          Forgot password?
+                        </button>
+                      </div>
+                      <div class="col-4">
+                        <p>Already have account?</p>
+                      </div>
+                      <div class="col-3">
+                        <button type="button" className="btn btn-primary">
+                          Sign Up
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
