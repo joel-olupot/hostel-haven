c391256a530c5556698deeaa4db94869bf9a370f
olupot-ikara <joel8tnz@gmail.com>
2023-11-02 17:44:57 +0300

    Created Booking page
    In this page details of a particular hostel will all be displayed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import HomePage from "./components/HomePage";
 import SignUp from "./components/SignUp";
+import Booking from "./components/BookingPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/booking" element={<Booking />} />
         </Routes>
       </div>
     </Router>
